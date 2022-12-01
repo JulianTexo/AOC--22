368612569daf529e37658572f332ed7dc8fcd5a2
JP <JulianTexo@gmail.com>
2022-12-01 16:31:03 +0100

    Moved from notebook to single files

---
+++ 1-12-solution.py
@@ -0,0 +1,24 @@
+with open('1-12-input.txt') as f:
+    contents = [line.rstrip() for line in f]
+
+inventories = []
+sum = 0
+for line in contents:
+    if line == "":
+        inventories.append(sum)
+        sum = 0
+    else:
+        line = line.replace('\n', '')
+        sum += int(line)
+
+max_inventory = max(inventories)
+
+print("Part one: " + str(max_inventory))
+
+TOP_COUNT = 3
+sum = 0
+for x in range(TOP_COUNT):
+    max_inventory = max(inventories)
+    sum += max_inventory
+    inventories.remove(max_inventory)
+print("Part two: " + str(sum))
